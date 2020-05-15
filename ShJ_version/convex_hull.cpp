181a6f81a768325a61263e8e1f3dcbdb1029b85d
Dmitrii Latyshev <LDDD5@yandex.ru>
2020-05-15 14:10:16 +0300

    shj: added convex hull

--- ShJ_version/convex_hull.cpp
+++ ShJ_version/convex_hull.cpp
@@ -6,14 +6,14 @@ pii Vector(pii head, pii tail) {
     return {tail.X - head.X, tail.Y - head.Y};
 }
 
-ll Cross(pii lhs, pii rhs) {
+long long Cross(pii lhs, pii rhs) {
     return 1LL * lhs.X * rhs.Y - 1LL * lhs.Y * rhs.X;
 }
 
 vector<pii> ConvexHull(vector<pii>& vec) {
-    sort(all(vec));
-    vec.erase(unique(all(vec)), vec.end());
-    if (sz(vec) == 1) {
+    sort(vec.begin(), vec.end());
+    vec.erase(unique(vec.begin(), vec.end()), vec.end());
+    if (vec.size() == 1) {
         return vec;
     }
     const pii pleft = vec.front();
@@ -21,15 +21,15 @@ vector<pii> ConvexHull(vector<pii>& vec) {
     vector<pii> up{pleft};
     vector<pii> down{pleft};
     for (int i = 1; i < sz(vec); ++i) {
-        const ll cp = Cross(Vector(pleft, vec[i]), Vector(vec[i], pright));
+        const long long cp = Cross(Vector(pleft, vec[i]), Vector(vec[i], pright));
         if (cp < 0 || i + 1 == sz(vec)) {
-            while (sz(up) > 1 && Cross(Vector(up[sz(up) - 2], vec[i]), Vector(up[sz(up) - 2], up.back())) <= 0) {
+            while (down.size() > 1 && Cross(Vector(up[up.size() - 2], vec[i]), Vector(up[up.size() - 2], up.back())) <= 0) {
                 up.pop_back();
             }
             up.push_back(vec[i]);
         }
         if (cp > 0 || i + 1 == sz(vec)) {
-            while(sz(down) > 1 && Cross(Vector(down[sz(down) - 2], vec[i]), Vector(down[sz(down) - 2], down.back())) >= 0) {
+            while (down.size() > 1 && Cross(Vector(down[down.size() - 2], vec[i]), Vector(down[down.size() - 2], down.back())) >= 0) {
                 down.pop_back();
             }
             down.push_back(vec[i]);
